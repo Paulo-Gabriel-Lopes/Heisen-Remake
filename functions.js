93a7f25e7718d4b39c97e700b1bb338856deb918
unknown <santoslopes960@gmail.com>
2025-10-03 00:10:30 -0300

    Adicionei a funcionalidade da mudança de texto toda vez que o heisenberg for clicado.

--- functions.js
+++ functions.js
@@ -1,5 +1,24 @@
-const imagemClick = document.querySelector('.heisenberg');
+let imagemClick = document.querySelector('.heisenberg');
+let textoCaixa = document.querySelector('.textoHeisen');
+
+const textInicial = textoCaixa.textContent;
+let textoMuda = 1;
 
 imagemClick.addEventListener('click', ()=>{
-    console.log("Hello!");
+    if(textoMuda == 1){
+        textoCaixa.textContent = "Hi, HI, HI, hi, hi";
+        textoMuda += 1;
+    } else if(textoMuda == 2){
+        textoCaixa.textContent = "Bah";
+        textoMuda += 1;
+    }else if(textoMuda == 3){
+        textoCaixa.textContent = "Boh";
+        textoMuda += 1;
+    }else if(textoMuda == 4){
+        textoCaixa.textContent = "Bih";
+        textoMuda = 0;
+    }else if(textoMuda == 0){
+        textoCaixa.textContent = textInicial;
+        textoMuda += 1;
+    }
 });
