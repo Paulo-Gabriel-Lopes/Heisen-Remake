4502842731abc795152bf194155bac0b90710912
unknown <santoslopes960@gmail.com>
2025-10-18 12:39:01 -0300

    Correção da responsividade em telas móveis quando estão na horizontal e a finalização do menu de hambueguer, adicionando sua funcionalidade

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 let imagemClick = document.querySelector('.heisenberg');
 let textoCaixa = document.querySelector('.textoHeisen');
 
+let clickerMenu = document.querySelector('.clicker');
+let burguer = document.querySelector('.wrapperBurguer')
+
 const textInicial = textoCaixa.innerHTML;
 let textoMuda = 1;
 
@@ -23,4 +26,9 @@ imagemClick.addEventListener('click', ()=>{
     }
 });
 
+clickerMenu.addEventListener('click', ()=>{
+    burguer.classList.toggle('hidden')
+    console.log('clicou')
+}); // Também é possivel utilizar o .add e .remove no classList, mas não coloque um . antes da classe
+    
 // textContent Apenas muda o textoCaixa, e Inner html consegue identificar tags como a </br>
